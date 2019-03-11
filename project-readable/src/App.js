8a3daf72849ae22cf4754b3ea9ce18e21c0de601
Runewather <victo_10_@hotmail.com>
2019-03-11 18:13:13 -0300

    Add post button component and router

--- project-readable/src/App.js
+++ project-readable/src/App.js
@@ -1,4 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import AddPost from './components/AddPost'
+import AddPostButton from './components/AddPostButton'
+import PostList from './components/PostList'
 import Post from './components/Post'
 import Button from './components/Button'
 import './App.css'
@@ -7,23 +11,26 @@ import './Normalize.css'
 class App extends Component {  
   render() {        
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="Title">Udacity React Readable</h1>         
-        </header>
-        <div className="Categories">
-          <Button name={"React"} />
-          <Button name={"Redux"} />
-          <Button name={"Udacity"} />
-        </div>
-        <div className="Wrapper">
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-        </div>
-      </div>
-    );
+      <Router>
+          <div className="App">
+            <header className="App-header">
+              <Link to={'/'} style={ { textDecoration: 'none', color: 'black'} }>
+                <h1 className="Title">React Blog</h1>   
+              </Link>      
+            </header>
+            <div className="Categories">
+              <Button name={"React"} />
+              <Button name={"Redux"} />
+              <Button name={"Udacity"} />
+            </div>
+            <div className="Wrapper">
+              <Route exact path='/' component={PostList} />
+              <Route exact path='/addPost' component={Post} />
+            </div>
+            <AddPostButton />
+          </div>
+      </Router>
+    )
   }
 }
 
